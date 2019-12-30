17d22d0207c5eeed79c79a80a583b1085738be40
YuanZheng Hu <bhu078@uottawa.ca>
2019-12-30 03:19:52 -0500

    add page

--- routes/adminpages.js
+++ routes/adminpages.js
@@ -1,9 +1,14 @@
 var express = require('express')
 var router = express.Router()
 
+var Page = require('../models/page')
 
 router.get('/',(req,res)=>{
-    res.send('admin area')
+    Page.find({}).sort({sorting:1}).exec((err,pages)=>{
+       res.render('admin/pages',{
+           pages:pages 
+       })
+   })
 })
 
 
@@ -31,9 +36,38 @@ router.post('/add-page',(req,res)=>{
     // body is body parser
     var title = req.body.title
     var slug = req.body.slug // .replace(/\s+g, '-').toLowercase()
-    if (slug == '') slug =title
+    if (slug == '') slug = title
  
     var content = req.body.content
+
+    Page.findOne({slug:slug},(error,page)=>{
+        if(page){
+            req.flash('danger','page slug exists choose another ')
+            res.render('admin/add_page',{
+                title,
+                slug,
+                content
+            })
+
+        }else{
+            var page = new Page({
+                title,
+                slug,
+                content, 
+                sorting : 100
+            })
+
+            page.save((error)=>{
+                if(error){
+                    return console.log(error)
+                }
+            
+                req.flash('success','page added!')
+                res.redirect('/admin/pages')
+            
+            })
+        }
+    })
 
     // var errors = req.validationErrors
 
@@ -42,6 +76,32 @@ router.post('/add-page',(req,res)=>{
     //     slug,
     //     content
     // })
+})
+
+// POST reorder pages 
+router.post('/reorder-pages',(req,res)=>{
+    var ids = req.body['id[]']
+
+    var count = 0 
+
+    for (var i = 0 ; i< ids.length ; i++){
+        var id = ids[i]
+        count++
+
+        (function(count){
+            Page.findById(id,(err,page) =>{
+                page.sorting = count 
+                page.save(function(err){
+                    if(err){
+                        return console.log(err)
+                    }
+                })
+            })
+
+        })(count)
+        
+
+    }
 })
 
 
